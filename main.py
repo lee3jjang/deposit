a4157e2a9fc49055fa261686fea886e151dc4bfb
SangJin <lee3jjang@gmail.com>
2021-06-25 17:54:52 +0900

    dfdfdfdf

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import time
 import pandas as pd
-from typing import List
+from typing import List, Dict
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
 driver = webdriver.Chrome(executable_path='chromedriver')
-# url = 'https://www.kfcc.co.kr/map/main.do'
+
+def get_regions() -> Dict[str, List[str]]:
+    """도시, 지역 리스트 수집
+
+    Returns:
+        Dict[str, List[str]]: 도시 -> (도시내)지역들 딕셔너리
+
+    Examples:
+        driver = webdriver.Chrome(executable_path='chromedriver')
+        regions = get_regions()
+        driver.close()
+    """
+    
+    url = 'https://www.kfcc.co.kr/map/main.do'
+    driver.get(url)
+    time.sleep(5)
+    for i in range(16):
+        driver.execute_script(f"regionSet({i+1})")
+        time.sleep(5)
+        result = {}
+        city = driver.find_element_by_css_selector('#main_right > div.result > div > span').text
+        regions = driver.find_element_by_css_selector('div.mapList > ul') \
+            .find_elements_by_tag_name('li')
+        regions = [region.text for region in regions]
+        result[city] = regions
+    return result
 
 def get_data(url: str) -> pd.DataFrame:
     """주어진 url에서 데이터 뽑아오기
@@ -55,6 +80,12 @@ def get_data(url: str) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    url = 'https://www.kfcc.co.kr/map/view.do?gmgoCd=2357&name=%EA%B0%95%ED%99%94&gmgoNm=%EA%B0%95%ED%99%94&divCd=001&divNm=%EB%B3%B8%EC%A0%90&gmgoType=%EC%A7%80%EC%97%AD&telephone=032-934-0071&fax=032-934-0074&addr=%EC%9D%B8%EC%B2%9C+%EA%B0%95%ED%99%94%EA%B5%B0+%EA%B0%95%ED%99%94%EC%9D%8D+%EA%B0%95%ED%99%94%EB%8C%80%EB%A1%9C+396-2&r1=%EC%9D%B8%EC%B2%9C&r2=%EA%B0%95%ED%99%94%EA%B5%B0&code1=2357&code2=001&sel=&key=&tab=sub_tab_rate'
-    df = get_data(url)
+
+    # Test 1
+    # regions = get_regions()
+
+    # # Test 2
+    # url = 'https://www.kfcc.co.kr/map/view.do?gmgoCd=2357&name=%EA%B0%95%ED%99%94&gmgoNm=%EA%B0%95%ED%99%94&divCd=001&divNm=%EB%B3%B8%EC%A0%90&gmgoType=%EC%A7%80%EC%97%AD&telephone=032-934-0071&fax=032-934-0074&addr=%EC%9D%B8%EC%B2%9C+%EA%B0%95%ED%99%94%EA%B5%B0+%EA%B0%95%ED%99%94%EC%9D%8D+%EA%B0%95%ED%99%94%EB%8C%80%EB%A1%9C+396-2&r1=%EC%9D%B8%EC%B2%9C&r2=%EA%B0%95%ED%99%94%EA%B5%B0&code1=2357&code2=001&sel=&key=&tab=sub_tab_rate'
+    # df = get_data(url)
+
     driver.close()
