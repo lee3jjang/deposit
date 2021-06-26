621cc764c12c07b564dfb88a8ee35d7a4457d89c
Sang-Jin <lee3jjang@gmail.com>
2021-06-26 22:14:33 +0900

    dfdfdf

--- data/결과조회.sql
+++ data/결과조회.sql
@@ -1,4 +1,10 @@
-select *
+select b.지점명, b.지역, b.상세지역, b.주소, a.상품군, a.상품명, a.계약기간, a.기본이율, b.URL
 	from 상품이율정보 a
 	left outer join 지점정보 b
-		on a.지점ID=b.지점ID;
+		on a.지점ID=b.지점ID
+	where 1=1
+		--and b.지역 = '인천'
+		--and b.상세지역 = '의정부'
+		and 기본이율 >= '연5.0%'
+	order by 기본이율 desc
+	--limit 20;
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ console = Console(file=report_file)
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
-options.add_argument("headless")
+# options.add_argument("headless")
 options.add_argument("disable-gpu")
 driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
@@ -62,6 +62,46 @@ def generate_tables():
     )
     """)
 
+
+# class SaemaulGeumgoCrawler:
+
+#     def __init__(self):
+#         options = webdriver.ChromeOptions()
+#         options.add_argument("--start-maximized")
+#         # options.add_argument("headless")
+#         options.add_argument("disable-gpu")
+#         self.driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+
+#     def get_prod_info(self, url: str) -> pd.DataFrame:
+#         # 화면접근
+#         self.driver.get(url)
+#         time.sleep(5)
+#         frame = self.driver.find_element_by_css_selector('div#sub_tab_rate > iframe#rateFrame')
+#         self.driver.switch_to.frame(frame)
+#         self.driver.execute_script("onSelectTab('14')")
+#         time.sleep(3)
+#         base_date = self.driver.find_element_by_css_selector('p.base-date').get_attribute('innerHTML')
+#         base_date = f"{base_date[6:10]}-{base_date[11:13]}-{base_date[14:16]}"
+
+#         # 수집시작
+#         result = []
+#         tablebox = self.driver.find_element_by_css_selector('div.table-box') \
+#             .find_elements_by_class_name('tblWrap')
+
+#         for table in tablebox:
+#             pdgr_name = table.find_element_by_css_selector('#divTmp1 > div.tbl-tit').get_attribute('innerHTML')
+#             rows = table.find_element_by_css_selector('#divTmp1 > table.rowTbl2 > tbody') \
+#                 .find_elements_by_tag_name('tr')
+#             for j, row in enumerate(rows):
+#                 if j==0:
+#                     prod_name = row.find_elements_by_tag_name('td')[0].get_attribute('innerHTML')
+#                 contract_period = row.find_elements_by_tag_name('td')[-2].get_attribute('innerHTML')
+#                 base_rate = row.find_elements_by_tag_name('td')[-1].get_attribute('innerHTML')
+#                 result.append([_generate_key(url), base_date, '적립식예탁금', pdgr_name, prod_name, contract_period, base_rate])
+#             time.sleep(3)
+#         result_df = pd.DataFrame(result, columns=['지점ID', '조회기준일', '상품유형', '상품군', '상품명', '계약기간', '기본이율'])
+        
+#         return result_df
 
 def get_region() -> Dict[str, List[str]]:
     """도시, 지역 리스트 수집
@@ -183,17 +223,22 @@ def get_prod_info(url: str) -> pd.DataFrame:
     return result_df
 
 
-def get_prod_info_batch(id, url):
-    today = datetime.now().strftime('%Y-%m-%d')
-    cur.execute(f"SELECT 1 FROM 상품이율정보 WHERE 지점ID='{id}' AND 조회기준일='{today}'")
-    if len(cur.fetchall()) > 0:
-        return
-    prod_info = get_prod_info(url)
-    if prod_info is None:
-        console.log(f'상품정보 없음 (지점ID: {id})')
-        return
-    prod_info.to_sql('상품이율정보', conn, if_exists='append', index=False)
-    console.log(f'상품이율정보 INSERT (지점ID: {id})')
+# def get_prod_info_batch(id, url):
+#     conn = sqlite3.connect('data/deposit.db')
+#     cur = conn.cursor()
+#     today = datetime.now().strftime('%Y-%m-%d')
+#     cur.execute(f"SELECT 1 FROM 상품이율정보 WHERE 지점ID='{id}' AND 조회기준일='{today}'")
+#     if len(cur.fetchall()) > 0:
+#         print("5")
+#         return
+#     crawler = SaemaulGeumgoCrawler()
+#     prod_info = crawler.get_prod_info(url)
+#     if prod_info is None:
+#         console.log(f'상품정보 없음 (지점ID: {id})')
+#         return
+#     print(prod_info.head())
+#     prod_info.to_sql('상품이율정보', conn, if_exists='append', index=False)
+#     console.log(f'상품이율정보 INSERT (지점ID: {id})')
 
 
 def _generate_key(url: str) -> str:
@@ -230,50 +275,50 @@ if __name__ == '__main__':
 
 
     # 지점정보 수집
-    for city in regions.keys():
-        for region in regions[city]:
-            cur = conn.cursor()
-            cur.execute(f"SELECT 1 FROM 지점정보 WHERE 지역='{city}' AND 상세지역='{region}'")
-            if len(cur.fetchall()) > 0:
-                continue
-            office_info = get_office_info(city, region)
-            if office_info is None:
-                console.log(f'지점정보 없음 (지역: {city}, 상세지역: {region})')
-                continue
-            office_info.to_sql('지점정보', conn, if_exists='append', index=False)
-            console.log(f'지점정보 INSERT (지역: {city}, 상세지역: {region})')
-    console.log(f'지점정보 수집 완료')
+    # for city in regions.keys():
+    #     for region in regions[city]:
+    #         cur = conn.cursor()
+    #         cur.execute(f"SELECT 1 FROM 지점정보 WHERE 지역='{city}' AND 상세지역='{region}'")
+    #         if len(cur.fetchall()) > 0:
+    #             continue
+    #         office_info = get_office_info(city, region)
+    #         if office_info is None:
+    #             console.log(f'지점정보 없음 (지역: {city}, 상세지역: {region})')
+    #             continue
+    #         office_info.to_sql('지점정보', conn, if_exists='append', index=False)
+    #         console.log(f'지점정보 INSERT (지역: {city}, 상세지역: {region})')
+    # console.log(f'지점정보 수집 완료')
 
     
     # 상품이율정보 수집
     cur = conn.cursor()
     cur.execute("SELECT 지점ID, URL FROM 지점정보")
-    # id_urls = cur.fetchall()
-    # for id_url in id_urls:
-    #     id, url = id_url
-    #     today = datetime.now().strftime('%Y-%m-%d')
-    #     cur.execute(f"SELECT 1 FROM 상품이율정보 WHERE 지점ID='{id}' AND 조회기준일='{today}'")
-    #     if len(cur.fetchall()) > 0:
-    #         continue
-    #     prod_info = get_prod_info(url)
-    #     if prod_info is None:
-    #         console.log(f'상품정보 없음 (지점ID: {id})')
-    #         continue
-    #     prod_info.to_sql('상품이율정보', conn, if_exists='append', index=False)
-    #     console.log(f'상품이율정보 INSERT (지점ID: {id})')
-    # console.log(f'상품이율정보 수집 완료')
-
-    
-    # 상품이율정보 수집 (multiprocessing)
-    cur = conn.cursor()
-    cur.execute("SELECT 지점ID, URL FROM 지점정보")
     id_urls = cur.fetchall()
     for id_url in id_urls:
         id, url = id_url
-        proc = Process(taret=get_prod_info_batch, args=(id, url))
-        proc.start()
-        proc.join()
+        today = datetime.now().strftime('%Y-%m-%d')
+        cur.execute(f"SELECT 1 FROM 상품이율정보 WHERE 지점ID='{id}' AND 조회기준일='{today}'")
+        if len(cur.fetchall()) > 0:
+            continue
+        prod_info = get_prod_info(url)
+        if prod_info is None:
+            console.log(f'상품정보 없음 (지점ID: {id})')
+            continue
+        prod_info.to_sql('상품이율정보', conn, if_exists='append', index=False)
+        console.log(f'상품이율정보 INSERT (지점ID: {id})')
     console.log(f'상품이율정보 수집 완료')
+
+    
+    # # 상품이율정보 수집 (multiprocessing)
+    # cur = conn.cursor()
+    # cur.execute("SELECT 지점ID, URL FROM 지점정보 WHERE 지점ID NOT IN (SELECT DISTINCT 지점ID FROM 상품이율정보)")
+    # id_urls = cur.fetchall()
+    # for id_url in id_urls:
+    #     id, url = id_url
+    #     proc = Process(target=get_prod_info_batch, args=(id, url))
+    #     proc.start()
+    #     proc.join()
+    # console.log(f'상품이율정보 수집 완료')
 
     report_file.close()
     driver.close()
