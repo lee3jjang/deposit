1644eeec04353452cc81777ecd63412118ae52f0
Sang-Jin <lee3jjang@gmail.com>
2021-06-26 10:26:13 +0900

    ddff

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ os.makedirs('result', exist_ok=True)
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
-# options.add_argument("headless")
+options.add_argument("headless")
 options.add_argument("disable-gpu")
 driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
 
