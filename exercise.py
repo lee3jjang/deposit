abc8da3f4176decedb641e7b3320af8f5efe464c
Sang-Jin <lee3jjang@gmail.com>
2021-06-27 11:54:09 +0900

    dfdf

--- exercise.py
+++ exercise.py
@@ -1,18 +1,8 @@
-# %%
-# Executor : 호출 가능한 객체를 비동기 호출
-# 함수나 코루틴 등을 실행시킴
-# ThreadPoolExecutor, ProcessPoolExecutor 방식으로 나뉨
-# submit, map, shutdown 3가지임
-
-# submit : 비동기 실행 함수 + 인자 => Future 클래스 반환
-# map : timeout 값 받을 수 있음 (넘으면 예외발생)
-# shutdown : 대기중인 Future 객체 down 시키고, 리소스 정리
-
-# %%
 import time
 import concurrent.futures
 from concurrent.futures import ProcessPoolExecutor, TimeoutError
 import logging
+from selenium import webdriver
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
@@ -21,40 +11,22 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-def worker(index, name):
-    logger.info(f'Worker Index(name={name}) : {index}')
+
+def worker(url):
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    logger.info(f'Worker Index({url}')
     time.sleep(1)
-    return f"Completed {index} worker job"
+    return url
+
 
 def main():
-    future_list = []
-    executor = ProcessPoolExecutor(max_workers=4)
-    for i in range(10):
-        future = executor.submit(worker, i, 'Sangjin')
-        future_list.append(future)
-    time.sleep(1)
-
-    for idx, future in enumerate(future_list):
-        if future.done():
-            logger.info(f"result: {future.result()}")
-            continue
-        logger.info("[%s worker] Wait for 1 second because it has not finished yet." % idx)
-        try:
-            result = future.result(timeout=10)
-        except TimeoutError:
-            logger.info("[%s worker] Timeout error" % idx)
-        else:
-            logger.info("result : %s" % result)
-    executor.shutdown(wait=False)
-
-
-def main2():
     with ProcessPoolExecutor(max_workers=4) as executor:
         future_list = []
-        for i in range(5):
-            future = executor.submit(worker, i, 'SangJin')
+        urls = ['https://www.naver.com/', 'https://github.com/', 'https://www.google.com/']
+        for url in urls:
+            future = executor.submit(worker, url)
             future_list.append(future)
-        finished, pending = concurrent.futures.wait(future_list, timeout=2, return_when=concurrent.futures.ALL_COMPLETED)
+        finished, pending = concurrent.futures.wait(future_list, timeout=10, return_when=concurrent.futures.ALL_COMPLETED)
 
         for w in finished:
             logger.info("Finished worker : %s" % w.result())
@@ -63,5 +35,4 @@ def main2():
         
 
 if __name__ == '__main__':
-    # main()
-    main2()
+    main()
